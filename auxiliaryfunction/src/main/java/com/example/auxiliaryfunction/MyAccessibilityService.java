39d353b224b30d6efb28ffb30ebd429a48920c78
lqwsy <2567924198@qq.com>
2018-03-22 18:01:02 +0800

    添加辅助功能开发module

---
+++ auxiliaryfunction/src/main/java/com/example/auxiliaryfunction/MyAccessibilityService.java
@@ -0,0 +1,24 @@
+package com.example.auxiliaryfunction;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Created by admin on 2018/3/22.
+ */
+
+public class MyAccessibilityService extends AccessibilityService {
+
+
+
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
